12942a16ef308c1044efd7e9b7fca906ae084868
Vincent Bernier <vincentbernier@hotmail.com>
2017-01-16 21:47:21 -0500

    Adding a projet to host the Service Registry Development

---
+++ SimpleServiceRegistry/ServiceRegistryDev/ServiceRegistryDev/ServiceRegistryDev.h
@@ -0,0 +1,19 @@
+//
+//  ServiceRegistryDev.h
+//  ServiceRegistryDev
+//
+//  Created by Vincent Bernier on 17-01-16.
+//  Copyright © 2017 Vincent Bernier. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ServiceRegistryDev.
+FOUNDATION_EXPORT double ServiceRegistryDevVersionNumber;
+
+//! Project version string for ServiceRegistryDev.
+FOUNDATION_EXPORT const unsigned char ServiceRegistryDevVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ServiceRegistryDev/PublicHeader.h>
+
+
