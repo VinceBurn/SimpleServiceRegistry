ab2f72549de7cf0d09ad2f0b6fd25eb8dfb511b3
Vincent Bernier <vincentbernier@hotmail.com>
2017-01-16 23:07:05 -0500

    Refactoring of test code

--- SimpleServiceRegistry/ServiceRegistryDev/ServiceRegistryDevTests/ServiceRegistry_test.swift
+++ SimpleServiceRegistry/ServiceRegistryDev/ServiceRegistryDevTests/ServiceRegistry_test.swift
@@ -12,34 +12,17 @@ import ServiceRegistryDev
 
 class ServiceRegistry_test: XCTestCase {
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    //MARK:-
-    func test_canInstantiate() {
-        _ = ServiceRegistry()
-    }
+    let sut = ServiceRegistry()
     
     //MARK:- Registering & Retrival
     func test_givenNoRegistration_whenSafeRetrival_thenOptionalOfTheRightType() {
-        let sut = ServiceRegistry()
         let optStr: String? = nil
-        let result = sut.safeService(for: String.self)
-        
+        let result = sut.safeService(for: String.self)        
         XCTAssertEqual(String(describing: type(of:result)), String(describing: type(of:optStr)))
     }
     
     func test_givenNewInstance_whenRegister_thenCompile() {
-        let sut = ServiceRegistry()
         let service = "Hello"
-        
         sut.register(service, for: String.self)
     }
     
